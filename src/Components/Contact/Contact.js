5f63dd60b378806af6d2ab4952417bc057b93f8d
sourabh7322 <sourabhrawat77200@gmail.com>
2024-07-27 14:34:22 +0530

    update in contact section

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -29,7 +29,7 @@ const Contact = () => {
 
     return (
         <>
-         <Typography 
+            <Typography 
                 variant='h2' 
                 sx={{ 
                     textAlign: "center", 
@@ -41,130 +41,148 @@ const Contact = () => {
             >
                 Contact Me
             </Typography>
-         <Box 
-            id="contactDiv" 
-            sx={{ 
-                width: { xs: '90%', md: '60%' }, 
-                margin: "10px auto", 
-                padding: "20px", 
-                backgroundColor: "#575555", 
-                borderRadius: "10px",
-                boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
-            }}
-        >
-           
-            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                <InputLabel 
-                    htmlFor="name" 
-                    sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
-                >
-                    Name
-                </InputLabel>
-                <TextField
-                    fullWidth
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
+            <Box 
+                id="contactDiv" 
+                sx={{ 
+                    width: { xs: '90%', md: '60%' }, 
+                    margin: "10px auto", 
+                    padding: "20px", 
+                    backgroundColor: "#333", 
+                    borderRadius: "10px",
+                    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+                }}
+            >
+                <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <InputLabel 
+                        htmlFor="name" 
+                        sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
+                    >
+                        Name
+                    </InputLabel>
+                    <TextField
+                        fullWidth
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        sx={{ 
+                            mb: 3, 
+                            backgroundColor: '#fff', 
+                            borderRadius: "5px",
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: '#bfd94a',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                            }
+                        }}
+                    />
+                    <InputLabel 
+                        htmlFor="email" 
+                        sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
+                    >
+                        Email
+                    </InputLabel>
+                    <TextField
+                        fullWidth
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        type="email"
+                        sx={{ 
+                            mb: 3, 
+                            backgroundColor: '#fff', 
+                            borderRadius: "5px",
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: '#bfd94a',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                            }
+                        }}
+                    />
+                    <InputLabel 
+                        htmlFor="message" 
+                        sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
+                    >
+                        Message
+                    </InputLabel>
+                    <TextField
+                        fullWidth
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        multiline
+                        rows={4}
+                        sx={{ 
+                            mb: 3, 
+                            backgroundColor: '#fff', 
+                            borderRadius: "5px",
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: '#bfd94a',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#00FF41',
+                                },
+                            }
+                        }}
+                    />
+                    <Button 
+                        type="submit" 
+                        variant="contained" 
+                        sx={{ 
+                            display: "block", 
+                            margin: "auto", 
+                            backgroundColor: "#00FF41", 
+                            color: "#333", 
+                            '&:hover': {
+                                backgroundColor: "#00e639",
+                            }
+                        }}
+                    >
+                        Send
+                    </Button>
+                </Box>
+                <Typography 
+                    variant='body1' 
                     sx={{ 
-                        mb: 3, 
-                        backgroundColor: '#fff', 
-                        borderRadius: "5px",
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: '#bfd94a',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                        }
-                    }}
-                />
-                <InputLabel 
-                    htmlFor="email" 
-                    sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
-                >
-                    Email
-                </InputLabel>
-                <TextField
-                    fullWidth
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    type="email"
-                    sx={{ 
-                        mb: 3, 
-                        backgroundColor: '#fff', 
-                        borderRadius: "5px",
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: '#bfd94a',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                        }
-                    }}
-                />
-                <InputLabel 
-                    htmlFor="message" 
-                    sx={{ color: "#00FF41", marginBottom: "5px", fontFamily: "monospace" }}
-                >
-                    Message
-                </InputLabel>
-                <TextField
-                    fullWidth
-                    id="message"
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    multiline
-                    rows={4}
-                    sx={{ 
-                        mb: 3, 
-                        backgroundColor: '#fff', 
-                        borderRadius: "5px",
-                        '& .MuiOutlinedInput-root': {
-                            '& fieldset': {
-                                borderColor: '#bfd94a',
-                            },
-                            '&:hover fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                            '&.Mui-focused fieldset': {
-                                borderColor: '#00FF41',
-                            },
-                        }
-                    }}
-                />
-                <Button 
-                    type="submit" 
-                    variant="contained" 
-                    sx={{ 
-                        display: "block", 
-                        margin: "auto", 
-                        backgroundColor: "#00FF41", 
-                        color: "#333", 
-                        '&:hover': {
-                            backgroundColor: "#00e639",
-                        }
+                        color: "#fff", 
+                        marginTop: "20px", 
+                        textAlign: "center",
+                        fontFamily: "monospace"
                     }}
                 >
-                    Send
-                </Button>
+                    Phone: 7906834867
+                </Typography>
+                <Typography 
+                    variant='body1' 
+                    sx={{ 
+                        color: "#fff", 
+                        marginTop: "10px", 
+                        textAlign: "center",
+                        fontFamily: "monospace"
+                    }}
+                >
+                    Email: sourabhrawat77200@gmail.com
+                </Typography>
             </Box>
-        </Box>
-        
-        
         </>
-       
     );
 };
 
